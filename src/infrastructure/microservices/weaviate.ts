bf49556f9c18bef62e5963a53b00b579b7fc326f
emmaalecrim <emmaalecrim@gmail.com>
2023-08-19 16:53:40 +0100

    inspector to weaviate up

--- src/infrastructure/microservices/weaviate.ts
+++ src/infrastructure/microservices/weaviate.ts
@@ -4,42 +4,45 @@ import weaviate, { WeaviateClient, ApiKey } from 'weaviate-ts-client';
 const client: WeaviateClient = weaviate.client({
     scheme: 'http',
     host: 'localhost:8080',
-    apiKey: new ApiKey(process.env.WEAVIATE_API_KEY || ""),  // Replace w/ your Weaviate instance API key
-    headers: { 'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY || "" },  // Replace with your inference API key
+    // apiKey: new ApiKey(process.env.WEAVIATE_API_KEY || ""),  // Replace w/ your Weaviate instance API key
+    // headers: { 'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY || "" },  // Replace with your inference API key
 });
 
-const classObj = {
-    "class": "Image",
-    "vectorizer": "img2vec-neural",
-    "vectorIndexConfig": {
-        "distance": "cosine",
-    },
-    "moduleConfig": {
-        "img2vec-neural": {
-            "imageFields": [
-                "image"
-            ]
-        }
-    },
-    "properties": [
-        {
-            "name": "image",
-            "dataType": ["blob"],
-        },
-        {
-            "name": "description",
-            "dataType": ["text"],
-            "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
-        },
-    ],
-}
+// (async () => {
+//     const res = await client.schema.classCreator().withClass({
+//         "class": "Image",
+//         "vectorizer": "img2vec-neural",
+//         "vectorIndexConfig": {
+//             "distance": "cosine",
+//         },
+//         "moduleConfig": {
+//             "img2vec-neural": {
+//                 "imageFields": [
+//                     "image"
+//                 ]
+//             }
+//         },
+//         "properties": [
+//             {
+//                 "name": "image",
+//                 "dataType": ["blob"],
+//             },
+//             {
+//                 "name": "description",
+//                 "dataType": ["text"],
+//                 "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+//             },
+//         ],
+//     }).do();
+//     console.debug(res);
+// })();
 
 
-
-async function addSchema() {
-    const res = await client.schema.classCreator().withClass(classObj).do();
+export async function addImage(image: string, description: string) {
+    const res = await client.data.creator().withClassName("Image").withProperties({ image, description }).do();
     console.debug(res);
 }
+
 
 
 
