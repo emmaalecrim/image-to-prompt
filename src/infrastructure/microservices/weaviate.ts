402f5c7003161e964d24528b159b1b5fa093d22a
emmaalecrim <emmaalecrim@gmail.com>
2023-08-20 15:17:43 +0100

    class creator dumb util

--- src/infrastructure/microservices/weaviate.ts
+++ src/infrastructure/microservices/weaviate.ts
@@ -8,65 +8,63 @@ const client: WeaviateClient = weaviate.client({
     // headers: { 'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY || "" },  // Replace with your inference API key
 });
 
-(async () => {
-    const res = await client.schema.classCreator().withClass({
-        "class": "Image",
-        "vectorizer": "multi2vec-clip",
-        "moduleConfig": {
-            "multi2vec-clip": {
-                "textFields": ["description"],
-                "imageFields": ["image"],
-            }
-        },
-        "properties": [
-            {
-                "name": "image",
-                "dataType": ["blob"],
-            },
-            {
-                "name": "description",
-                "dataType": ["text"],
-                "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
-            }
-        ],
-    }).do();
-    console.debug(res);
-})();
+// (async () => {
+// const res = await client.schema.classCreator().withClass({
+//     "class": "Image",
+//     "vectorizer": "multi2vec-clip",
+//     "moduleConfig": {
+//         "multi2vec-clip": {
+//             "textFields": ["description"],
+//             "imageFields": ["image"],
+//         }
+//     },
+//     "properties": [
+//         {
+//             "name": "image",
+//             "dataType": ["blob"],
+//         },
+//         {
+//             "name": "description",
+//             "dataType": ["text"],
+//             "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+//         }
+//     ],
+// }).do();
+// await client.schema.classCreator().withClass({
+//     "class": "GeneratedImage",
+//     "vectorizer": "multi2vec-clip",
+//     "moduleConfig": {
+//         "multi2vec-clip": {
+//             "textFields": ["description"],
+//             "imageFields": ["image"],
+//         }
+//     },
+//     "properties": [
+//         {
+//             "name": "image",
+//             "dataType": ["blob"],
+//         },
+//         {
+//             "name": "description",
+//             "dataType": ["text"],
+//             "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+//         },
+//         {
+//             "name": "generation",
+//             "dataType": ["number"],
+//             "description": "How many generations this image is from the original image.",
+//         },
+//         {
+//             "name": "baseImage",
+//             "dataType": ["Image"],
+//             "description": "The original image and prompt that were used to generate this image.",
+//         }
+//     ],
+// }).do();
 
-(async () => {
-    const res = await client.schema.classCreator().withClass({
-        "class": "GeneratedImage",
-        "vectorizer": "multi2vec-clip",
-        "moduleConfig": {
-            "multi2vec-clip": {
-                "textFields": ["description"],
-                "imageFields": ["image"],
-            }
-        },
-        "properties": [
-            {
-                "name": "image",
-                "dataType": ["blob"],
-            },
-            {
-                "name": "description",
-                "dataType": ["text"],
-                "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
-            },
-            {
-                "name": "generation",
-                "dataType": ["number"],
-                "description": "How many generations this image is from the original image.",
-            },
-            {
-                "name": "baseImage",
-                "dataType": ["Image"],
-                "description": "The original image and prompt that were used to generate this image.",
-            }
-        ],
-    }).do();
-    console.debug(res);
-})();
+
+// })();
+
 
 
 
