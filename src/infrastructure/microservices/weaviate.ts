758f3e20c05075f7d16b310bddc4ce6e3192be47
emmaalecrim <emmaalecrim@gmail.com>
2023-08-20 20:58:28 +0100

    fixed ref creation, working imgranking and ref2vec

--- src/infrastructure/microservices/weaviate.ts
+++ src/infrastructure/microservices/weaviate.ts
@@ -8,107 +8,119 @@ const client: WeaviateClient = weaviate.client({
     // headers: { 'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY || "" },  // Replace with your inference API key
 });
 
-// (async () => {
-// const res = await client.schema.classCreator().withClass({
-//     "class": "Image",
-//     "vectorizer": "multi2vec-clip",
-//     "moduleConfig": {
-//         "multi2vec-clip": {
-//             "textFields": ["description"],
-//             "imageFields": ["image"],
-//         }
-//     },
-//     "properties": [
-//         {
-//             "name": "image",
-//             "dataType": ["blob"],
-//         },
-//         {
-//             "name": "description",
-//             "dataType": ["text"],
-//             "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
-//         }
-//     ],
-// }).do();
-// await client.schema.classCreator().withClass({
-//     "class": "GeneratedImage",
-//     "vectorizer": "multi2vec-clip",
-//     "moduleConfig": {
-//         "multi2vec-clip": {
-//             "textFields": ["description"],
-//             "imageFields": ["image"],
-//         }
-//     },
-//     "properties": [
-//         {
-//             "name": "image",
-//             "dataType": ["blob"],
-//         },
-//         {
-//             "name": "description",
-//             "dataType": ["text"],
-//             "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
-//         },
-//         {
-//             "name": "generation",
-//             "dataType": ["number"],
-//             "description": "How many generations this image is from the original image.",
-//         },
-//         {
-//             "name": "baseImage",
-//             "dataType": ["Image"],
-//             "description": "The original image and prompt that were used to generate this image.",
-//         }
-//     ],
-// }).do();
-
-
-// })();
+(async () => {
+    // const res = await client.schema.classCreator().withClass({
+    //     "class": "Image",
+    //     "vectorizer": "multi2vec-clip",
+    //     "moduleConfig": {
+    //         "multi2vec-clip": {
+    //             "textFields": ["description"],
+    //             "imageFields": ["image"],
+    //         }
+    //     },
+    //     "properties": [
+    //         {
+    //             "name": "image",
+    //             "dataType": ["blob"],
+    //         },
+    //         {
+    //             "name": "description",
+    //             "dataType": ["text"],
+    //             "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+    //         }
+    //     ],
+    // }).do();
+    // await client.schema.classCreator().withClass({
+    //     "class": "GeneratedImage",
+    //     "vectorizer": "multi2vec-clip",
+    //     "moduleConfig": {
+    //         "multi2vec-clip": {
+    //             "textFields": ["description"],
+    //             "imageFields": ["image"],
+    //         },
+    //         "ref2vec-centroid": {
+    //             "referenceProperties": ["hasParagraphs"],
+    //             "method": "mean"
+    //         }
+    //     },
+    //     "properties": [
+    //         {
+    //             "name": "image",
+    //             "dataType": ["blob"],
+    //         },
+    //         {
+    //             "name": "description",
+    //             "dataType": ["text"],
+    //             "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+    //         },
+    //         {
+    //             "name": "generation",
+    //             "dataType": ["number"],
+    //             "description": "How many generations this image is from the original image.",
+    //         },
+    //         {
+    //             "name": "baseImage",
+    //             "dataType": ["Image"],
+    //             "description": "The original image and prompt that were used to generate this image.",
+    //         }
+    //     ],
+    // }).do();
+})();
 
 
 
 
 
 export async function addImage({ image, description }: { image: Blob, description: string }) {
-    const res = await client.data.creator().withClassName("Image").withProperties({ image, description }).do();
+    const res = await client.data.creator().withClassName("Image").withProperties({ image, description }).withId(generateUuid5(image.toString())).do();
     return res
 }
 
 export async function addGenerationBatch(dataObjs: {
-    image: Blob, description: string, generation?: number, baseImageId: string
-}[]) {
+    image: Blob, description: string
+}[], { generation, baseImageId }: { generation?: number, baseImageId: string }) {
     // Create batch
     let batcher = client.batch.objectsBatcher();
-    let referenceBatcher = client.batch.referencePayloadBuilder();
-    for (const dataObj of dataObjs)
-        batcher = batcher.withObject({
-            class: "ImageGeneration",
-            properties: dataObj,
-            id: generateUuid5(dataObj.image.toString()),
-        })
-    // Flush
-    const batchRes = await batcher.do();
-    console.log(JSON.stringify(batchRes, null, 2))
-    // Create references
+
     for (const dataObj of dataObjs) {
-        referenceBatcher
-            .withFromClassName("ImageGeneration")
-            .withFromId(generateUuid5(dataObj.image.toString()))
-            .withToClassName("Image")
-            .withToId(dataObj.baseImageId)
-            .do();
+        const obj = client.data.creator()
+            .withClassName("GeneratedImage")
+            .withProperties({ image: dataObj.image, description: dataObj.description, generation })
+            .withId(generateUuid5(dataObj.image.toString()))
+            .payload()
+        const ref = client.data
+            .referenceCreator()
+            .withClassName('GeneratedImage')
+            .withId(generateUuid5(dataObj.image.toString()))
+            .withReferenceProperty('baseImage')
+            .withReference(
+                client.data
+                    .referencePayloadBuilder()
+                    .withClassName('Image')
+                    .withId(baseImageId)
+                    .payload()
+            ).payload()
+        batcher = batcher.withObjects(obj, ref)
     }
     // Flush
-    const referenceRes = await referenceBatcher.do();
-    console.log(JSON.stringify(referenceRes, null, 2))
+    return batcher.do();
 }
 
 export async function getRankedImages(id: string) {
+    const originalVec = await client.data
+        .getterById()
+        .withClassName('Image')
+        .withId(id)
+        .withVector()
+        .do();
     const res = await client.graphql
         .get()
-        .withClassName('ImageGeneration')
-        .withNearObject({ id })
-        .withFields('description _additional { distance generation id }')
+        .withNearVector({
+            // @ts-ignore - afaf
+            vector: originalVec?.vector,
+        })
+        .withClassName('GeneratedImage')
+        .withFields('description generation _additional { certainty distance }')
         .do();
 
     return res
