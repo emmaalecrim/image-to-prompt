7690ac055c94c0a008a14170ed43db9b37dcf82a
emmaalecrim <emmaalecrim@gmail.com>
2023-08-20 15:14:22 +0100

    GeneratedImage schema and relationship

--- src/infrastructure/microservices/index.ts
+++ src/infrastructure/microservices/index.ts
@@ -2,7 +2,8 @@ import express from 'express'
 import fetch from 'node-fetch'
 import bodyParser from 'body-parser'
 import { inspectImg } from './replicate.ts'
-import { addImage } from './weaviate.ts'
+import { addImage, getRankedImages, addGenerationBatch } from './weaviate.ts'
+import { getImages } from './openai.ts'
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,22 +14,44 @@ const port = 8001
 app.post('/add-generation', async (req, res) => {
     try {
         const { url } = req.body
-        // TODO: setup img upload to bucket, get url. Keep img aroud for process.
-        // 1. Call inspector microservice to decribe image
+        // TODO: setup img upload to Storage bucket and return url
+        // 1. Call inspector to decribe image once you have the url and upload to weaviate
         console.debug(`Inspecting image at ${url}`)
-        const [prompt, image] = await Promise.all([inspectImg(url), fetchImg(url)].map(p => p.catch(e => e)))
+        const [description, image] = await Promise.all([inspectImg(url), fetchImg(url)].map(p => p.catch(e => e)))
         // 2. Add Goal Image to Weaviate(with generated prompt)
-        addImage(image, prompt).then((res) => console.debug(`Added image to Weaviate`, res))
-        // 3. Get generations from DALL-E using the prompt
-        // 4. Describe images returned by DALL-E
-        // 5. Upload generated images to weaviate
-        // 6. Return prompt and distance values
-        // use agi to improve prompts based on user/system feedback
-        // TODO: Setup GCP bucket to store images
-        // TODO: Setup cloudbuild to deploy to GCP
-        // TODO: Setup GCP cloud run to run microservices
-        // TODO: Setup multi-tenancy
-        return res.send(prompt)
+        const object = await addImage({ image, description })
+        console.debug(`Added image to Weaviate with id ${object.id}`)
+        return res.status(200).json({ id: object.id, prompt: description })
+    } catch (e: any) {
+        console.error(e.message)
+        res.send(e.message)
+    }
+})
+
+// TODO: Setup GCP bucket to store images
+// TODO: Setup cloudbuild to deploy to GCP
+// TODO: Setup GCP cloud run to run microservices
+// TODO: Setup multi-tenancy
+
+// 3. Get generations from DALL-E using the prompt
+// 4. Describe images returned by DALL-E
+// 5. Upload generated images to weaviate
+// 6. Get distance values
+// 7. Return { prompt, id, distance } to client
+// use agi to improve prompts based on user/system feedback
+
+app.post('/iterate-generation', async (req, res) => {
+    const { prompt, id, generation } = req.body
+    try {
+        const generations = await getImages(prompt)
+        console.log(JSON.stringify(generations, null, 2))
+        const descriptions = await Promise.all(generations.map((url) => inspectImg(url).catch(e => e)))
+        console.debug(`Descriptions: ${JSON.stringify(descriptions, null, 2)}`)
+        const images = await Promise.all(generations.map((url) => fetchImg(url).catch(e => e)))
+        await addGenerationBatch(images.map((url, i) => ({ image: url, description: descriptions[i], generation, baseImageId: id })))
+        const rankedImages = await getRankedImages(id)
+        console.log(JSON.stringify(rankedImages, null, 2))
+        return res.status(200).json(rankedImages)
     } catch (e: any) {
         console.error(e.message)
         res.send(e.message)
--- src/infrastructure/microservices/weaviate.ts
+++ src/infrastructure/microservices/weaviate.ts
@@ -1,4 +1,4 @@
-import weaviate, { WeaviateClient, ApiKey } from 'weaviate-ts-client';
+import weaviate, { WeaviateClient, ApiKey, generateUuid5 } from 'weaviate-ts-client';
 
 
 const client: WeaviateClient = weaviate.client({
@@ -8,40 +8,114 @@ const client: WeaviateClient = weaviate.client({
     // headers: { 'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY || "" },  // Replace with your inference API key
 });
 
-// (async () => {
-//     const res = await client.schema.classCreator().withClass({
-//         "class": "Image",
-//         "vectorizer": "img2vec-neural",
-//         "vectorIndexConfig": {
-//             "distance": "cosine",
-//         },
-//         "moduleConfig": {
-//             "img2vec-neural": {
-//                 "imageFields": [
-//                     "image"
-//                 ]
-//             }
-//         },
-//         "properties": [
-//             {
-//                 "name": "image",
-//                 "dataType": ["blob"],
-//             },
-//             {
-//                 "name": "description",
-//                 "dataType": ["text"],
-//                 "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
-//             },
-//         ],
-//     }).do();
-//     console.debug(res);
-// })();
-
-
-export async function addImage(image: string, description: string) {
-    const res = await client.data.creator().withClassName("Image").withProperties({ image, description }).do();
+(async () => {
+    const res = await client.schema.classCreator().withClass({
+        "class": "Image",
+        "vectorizer": "multi2vec-clip",
+        "moduleConfig": {
+            "multi2vec-clip": {
+                "textFields": ["description"],
+                "imageFields": ["image"],
+            }
+        },
+        "properties": [
+            {
+                "name": "image",
+                "dataType": ["blob"],
+            },
+            {
+                "name": "description",
+                "dataType": ["text"],
+                "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+            }
+        ],
+    }).do();
     console.debug(res);
+})();
+
+(async () => {
+    const res = await client.schema.classCreator().withClass({
+        "class": "GeneratedImage",
+        "vectorizer": "multi2vec-clip",
+        "moduleConfig": {
+            "multi2vec-clip": {
+                "textFields": ["description"],
+                "imageFields": ["image"],
+            }
+        },
+        "properties": [
+            {
+                "name": "image",
+                "dataType": ["blob"],
+            },
+            {
+                "name": "description",
+                "dataType": ["text"],
+                "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+            },
+            {
+                "name": "generation",
+                "dataType": ["number"],
+                "description": "How many generations this image is from the original image.",
+            },
+            {
+                "name": "baseImage",
+                "dataType": ["Image"],
+                "description": "The original image and prompt that were used to generate this image.",
+            }
+        ],
+    }).do();
+    console.debug(res);
+})();
+
+
+
+
+export async function addImage({ image, description }: { image: Blob, description: string }) {
+    const res = await client.data.creator().withClassName("Image").withProperties({ image, description }).do();
+    return res
 }
+
+export async function addGenerationBatch(dataObjs: {
+    image: Blob, description: string, generation?: number, baseImageId: string
+}[]) {
+    // Create batch
+    let batcher = client.batch.objectsBatcher();
+    let referenceBatcher = client.batch.referencePayloadBuilder();
+    for (const dataObj of dataObjs)
+        batcher = batcher.withObject({
+            class: "ImageGeneration",
+            properties: dataObj,
+            id: generateUuid5(dataObj.image.toString()),
+        })
+    // Flush
+    const batchRes = await batcher.do();
+    console.log(JSON.stringify(batchRes, null, 2))
+    // Create references
+    for (const dataObj of dataObjs) {
+        referenceBatcher
+            .withFromClassName("ImageGeneration")
+            .withFromId(generateUuid5(dataObj.image.toString()))
+            .withToClassName("Image")
+            .withToId(dataObj.baseImageId)
+            .do();
+    }
+    // Flush
+    const referenceRes = await referenceBatcher.do();
+    console.log(JSON.stringify(referenceRes, null, 2))
+}
+
+export async function getRankedImages(id: string) {
+    const res = await client.graphql
+        .get()
+        .withClassName('ImageGeneration')
+        .withNearObject({ id })
+        .withFields('description _additional { distance generation id }')
+        .do();
+
+    return res
+}
+
 
 
 
