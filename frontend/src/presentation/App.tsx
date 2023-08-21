028b06ebe1de7941baaae572e58a30969ccdec35
Luis Pires <luis.pires@peekmed.com>
2023-08-21 10:01:37 +0100

    added doc

--- frontend/src/presentation/App.tsx
+++ frontend/src/presentation/App.tsx
@@ -1,10 +1,18 @@
+// React Imports
 import React from 'react';
+// Component Imports
 import Main from '@components/main';
 import Header from './components/header';
+// Assets Imports
 import '@assets/fonts/PressStart2P-Regular.ttf';
 import '@sass/main.scss';
 import './App.scss';
-
+/**
+ *
+ *  Constains:
+ * - Header: Component present throughout the app, contains app logo
+ * - Main: Component responsible for the flow of the entire app
+ */
 export default function App() {
   return (
     <div>
