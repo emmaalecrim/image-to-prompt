f3a6540fcc24ac643ca4209f9e19dd1bef1a7440
emmaalecrim <emmaalecrim@gmail.com>
2023-08-21 13:15:53 +0100

    removed volume, moved classes into weaviate file

--- services/src/utils/weaviate.ts
+++ services/src/utils/weaviate.ts
@@ -62,6 +62,76 @@ export async function getRankedImages(id: string) {
 }
 
 
+(async () => {
+    console.info("Creating classes")
+
+    await client.schema.classCreator().withClass({
+        "class": "Image",
+        "vectorizer": "multi2vec-clip",
+        "moduleConfig": {
+            "multi2vec-clip": {
+                "textFields": ["description"],
+                "imageFields": ["image"],
+            }
+        },
+        "properties": [
+            {
+                "name": "image",
+                "dataType": ["blob"],
+            },
+            {
+                "name": "description",
+                "dataType": ["text"],
+                "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+            },
+        ],
+    }).do().catch((e) => console.info("Image class already exists"));
+
+    await client.schema.classCreator().withClass({
+        "class": "GeneratedImage",
+        "vectorizer": "multi2vec-clip",
+        "moduleConfig": {
+            "multi2vec-clip": {
+                "textFields": ["description"],
+                "imageFields": ["image"],
+            },
+            "ref2vec-centroid": {
+                "referenceProperties": ["hasParagraphs"],
+                "method": "mean"
+            }
+        },
+        "properties": [
+            {
+                "name": "image",
+                "dataType": ["blob"],
+            },
+            {
+                "name": "description",
+                "dataType": ["text"],
+                "description": "Text description of the image. Can be a prompt that generates this image or it's img2text description.",
+            },
+            {
+                "name": "generation",
+                "dataType": ["number"],
+                "description": "How many generations this image is from the original image.",
+            },
+            {
+                "name": "baseImage",
+                "dataType": ["Image"],
+                "description": "The original image and prompt that were used to generate this image.",
+
+            },
+            {
+                "name": "url",
+                "dataType": ["string"],
+                "description": "The url of the image.",
+
+            }
+        ],
+    }).do().catch((e) => console.info("GeneratedImage class already exists"));
+    console.info("Successfully created classes!")
+})();
+
 
 
 
